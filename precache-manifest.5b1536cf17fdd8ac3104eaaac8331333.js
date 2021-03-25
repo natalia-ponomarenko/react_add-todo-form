04b44cd93314160ab995e6c8b969fa537c3f3e92
Ping <4ivrik@ukr.net>
2021-03-25 19:26:58 +0200

    Updates

---
+++ precache-manifest.5b1536cf17fdd8ac3104eaaac8331333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376fb581d79be9cecdd5d05bcf12dbcf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff018e67bf4cf226eec3",
+    "url": "./static/css/main.2fc04ec1.chunk.css"
+  },
+  {
+    "revision": "e1379c20889687693a10",
+    "url": "./static/js/2.18044a14.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18044a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff018e67bf4cf226eec3",
+    "url": "./static/js/main.199cc63c.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
